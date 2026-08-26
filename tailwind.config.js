3e55d97cc16f4eae2b44ea8a99a3eba2f42a2bf8
AA-NBTI <anbtiteam@gmail.com>
2026-08-26 14:28:16 +0900

    fix: switch to TailwindCSS v3 postcss pipeline for perfect UI styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-'use me'
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,4 +9,4 @@ export default {
     extend: {},
   },
   plugins: [],
-} satisfies Config;
+}
